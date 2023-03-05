808246bf0481d1fde9542c2873d59eb195ff1148
Alexsand74 <1974alexsand@gmail.com>
2023-03-05 13:30:46 +0300

    Homemade Rear 1.4 Add getALL Arreys invocation

--- src/main/java/com/urise/webapp/storage/ArrayStorage.java
+++ src/main/java/com/urise/webapp/storage/ArrayStorage.java
@@ -2,15 +2,17 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.Arrays;
+
 public class ArrayStorage {
     private final Resume[] storage = new Resume[10000];
     private int size = 0;
-     public void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
+
     public void update(Resume r) {
         int index = getIndex(r.getUuid());
         if (index == -1) {
@@ -19,9 +21,10 @@ public class ArrayStorage {
             storage[index] = r;
         }
     }
+
     public void update(Resume oldResume, Resume resume) {
         int i = getIndex(oldResume.getUuid());
-        if (i == -1){
+        if (i == -1) {
             System.out.println(" Resume " + oldResume.getUuid() + " not exist ");
         } else {
             storage[i] = resume;
@@ -30,7 +33,7 @@ public class ArrayStorage {
 
     public void save(Resume r) {
         int i = getIndex(r.getUuid());
-        if (i != -1 || size == storage.length){
+        if (i != -1 || size == storage.length) {
             System.out.println(" Resume " + r.getUuid() + " already exist ");
         } else {
             storage[size] = r;
@@ -63,9 +66,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] returnResult = new Resume[size];
-        System.arraycopy(storage, 0, returnResult, 0, size);
-        return returnResult;
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     public int size() {
